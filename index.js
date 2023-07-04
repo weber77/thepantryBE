3aa4dfd4b9d97d47747462cb0987442911e0e075
weber77 <weberkanga77@gmail.com>
2023-07-05 00:33:08 +0100

    added vercel.json

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const express = require("express");
-
 const app = express();
-const PORT = 4000;
+const port = process.env.PORT || 4040;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(require("cors")());
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
+(async function () {
+  try {
+    await require("mongoose").connect(
+      "mongodb+srv://purpleinkpen:purpleinkpen@cluster0.ixwji.mongodb.net/thePantryApp?retryWrites=true&w=majority",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    );
+    console.log("Connected to the DB");
+  } catch (err) {
+    console.log(
+      "ERROR: Seems like your DB is not running, please start it up !!!"
+    );
+  }
+})();
+
+app.use("/user", require("../routes/userRoutes"));
+
+const path = require("path");
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("/*", function (req, res) {
+  //   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.send("Hi you Some more text😃");
 });
 
-app.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
-});
-
-app.get("/about", (req, res) => {
-  res.send("This is my about route..... ");
-});
-
-// Export the Express API
-module.exports = app;
+app.listen(port, () => console.log(`Listening on port: ${port}`));
