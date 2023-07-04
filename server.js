24e60ac15fc5909e7e4819676ddcb552398176af
weber77 <weberkanga77@gmail.com>
2023-07-05 00:45:02 +0100

    added start

--- server.js
+++ server.js
@@ -13,4 +13,35 @@ app.get("/api/item/:slug", (req, res) => {
   res.end(`Item: ${slug}`);
 });
 
+app.get("/api/register", async (req, res) => {
+  const { email, password, password2 } = req.body;
+  if (!email || !password || !password2)
+    return res.json({ ok: false, message: "All fields required" });
+  if (password !== password2)
+    return res.json({ ok: false, message: "Passwords must match" });
+  if (!validator.isEmail(email))
+    return res.json({ ok: false, message: "Invalid credentials" });
+  try {
+    const user = await User.findOne({ email });
+    if (user) return res.json({ ok: false, message: "Invalid credentials" });
+    const hash = await argon2.hash(password);
+    console.log("hash ==>", hash);
+    const newUser = {
+      email,
+      password: hash,
+      username: email,
+    };
+    const newuser = await User.create(newUser);
+    const token = jwt.sign(newuser.toJSON(), jwt_secret, { expiresIn: "7d" });
+    res.json({
+      ok: true,
+      message: "Successfully registered",
+      token,
+      user: newuser,
+    });
+  } catch (err) {
+    res.json({ ok: false, err });
+  }
+});
+
 module.exports = app;
